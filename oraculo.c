0d850d9bc590b048459e598ec8d9fe698cb5e58c
afzdev13 <joaosilvaafonso14@gmail.com>
2019-12-10 00:02:26 -0400

    Initial commit

---
+++ oraculo.c
@@ -0,0 +1,32 @@
+int microfone = 8;
+ 
+int CONT = 0; 
+ 
+void setup() { //rotina ao iniciar o programa 
+ pinMode(microfone, INPUT); //inicia os pino do microfone como entrada
+ pinMode(13, OUTPUT); //led vermelho
+ pinMode(12, OUTPUT); //led verde
+
+ Serial.begin(9600);
+ // Se o pino de entrada analógica 0 é deixado desconectado,
+ // o ruído aleatório analógico irá causar a chamada de randomSeed()
+ //  gerar sementes aleatórias diferentes cada vez que o sketch roda.
+ // randomSeed() basicamente "embaralha" a função random().
+ randomSeed(analogRead(0));
+}
+ 
+void loop() { //rotina infinita
+
+    if(digitalRead(microfone) == HIGH){
+        CONT = 1;
+    }
+    if(digitalRead(microfone) == LOW && CONT == 1 ){
+        randNumber = random(300);
+        if(randNumber%2 == 0){
+            digitalWrite(13, HIGH);
+        } else {
+            digitalWrite(12, HIGH);
+        }
+    }
+
+}
